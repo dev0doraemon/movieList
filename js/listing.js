d08c740dc70a26da405991373c51588b59098871
Dong Hyeon Oh <dev0doraemon@gmail.com>
2023-05-31 19:51:43 +0900

    some css styles was changed

--- js/listing.js
+++ js/listing.js
@@ -3,26 +3,33 @@ let page = 1;
 let movie_url = movie_base_url + page;
 
 const image_base_url = "https://image.tmdb.org/t/p/";
-
 let size = "w500"; // "original" is available
 
+// 검색 수행하는 함수
 let handleSearch = () => {
+    // 포스트 제목 정보를 불러오기 위해 
     let posts = document.getElementsByClassName('movie_post');
     // console.log(posts);
     // foreach 왜 안되누..
     // posts.forEach((post) => {
     // })
     let title;
+    // 검색할 때 쓴 문자열 정보를 불러오고 모두 소문자로 바꿈
     let keyword = document.getElementById('search_title').value.toLowerCase();
+    // 포스트 내용 하나씩 비교
     for (let i = 0; i < posts.length; i++) {
+        // 제목의 텍스트를 소문자로 바꿈
         title = posts[i].children[1].textContent.toLowerCase();
+
         if (title.indexOf(keyword) === -1) {
+            // 타이틀과 검색 키워드가 일치하는 패턴이 없을 경우 포스터 안보여줌
             posts[i].style.display = "none";
         }
         else {
+            // 타이틀과 검색 키워드가 일치하는 패턴이 있을 경우 포스터 보여줌
             posts[i].style.display = "block";
         }
-        console.log(posts[i]);
+        // console.log(posts[i]);
     }
 }
 
@@ -41,6 +48,14 @@ let getMovieData = async (url) => {
 }
 
 let renderMovieTemplate = (postId, postImg, postTitle, postOverView, postVoteAvg) => {
+    // innerHTML 사용법 몰라서 이렇게 적었심다...
+    // 자바스크립트 코드는 아래 html로 나타낸 코드 형태와 같음
+    // `<div class="movie_post" id=${postId}>
+    //     <img class="movie_poster" src=${postImg} />
+    //     <h2 class="title">${postTitle}</h2>
+    //     <div class="overview">${postOverView}</div>
+    //     <div class="voteAvg">score : ${postVoteAvg}</div>
+    // </div>`
     const main = document.querySelector('main');
     let divPost = document.createElement('div');
     divPost.setAttribute('id', postId);
@@ -48,13 +63,16 @@ let renderMovieTemplate = (postId, postImg, postTitle, postOverView, postVoteAvg
     
     let img = document.createElement('img');
     img.src = postImg;
+    img.setAttribute('class', 'movie_poster');
     let title = document.createElement('h2');
     title.setAttribute('class', 'title');
     title.textContent = postTitle;
     let overview = document.createElement('div');
+    overview.setAttribute('class', 'overview');
     overview.textContent = postOverView;
-    let voteAvg = document.createElement('span');
-    voteAvg.textContent = postVoteAvg;
+    let voteAvg = document.createElement('div');
+    voteAvg.setAttribute('class', 'voteAvg');
+    voteAvg.textContent = `score : ${postVoteAvg}`;
     // divPost.textContent = postId;
     
     main.appendChild(divPost);
@@ -66,6 +84,17 @@ let renderMovieTemplate = (postId, postImg, postTitle, postOverView, postVoteAvg
     });
 }
 
+// 문서가 처음 시작할 때 실행되는 코드
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('search_title').addEventListener("keydown", (e) => {
+        if (e.key || e.keyCode === 'Enter') {
+            handleSearch();
+        }
+    });
+    getMovieData(movie_url);
+});
+
+// 영화 정보를 불러오는 코드
 getMovieData(movie_url).then((data) => {
     // console.log(data);
     let posts = data['results'];
@@ -74,3 +103,52 @@ getMovieData(movie_url).then((data) => {
         renderMovieTemplate(post['id'], image_base_url + size + post['poster_path'], post['original_title'], post['overview'], post['vote_average']);
     });
 });
+
+
+
+
+
+// const options = {
+//     method: "GET",
+//     headers: {
+//         accept: "application/json",
+//         Authorization:
+//             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjQxMWFmZjRiODc0OTE1MWQ1ZDBjZDAyZTIzNGVmZSIsInN1YiI6IjY0NzA4NmZmMzM2ZTAxMDBjNzA3OTc1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NSCtfR7Cu5h3HYuAUk_fJ1ivWxZ8ewChQOmBNjM2VQo",
+//     },
+// };
+
+// fetch(url + page, options)
+//     .then((response) => response.json())
+//     .then((response) => console.log(response))
+//     .catch((err) => console.error(err));
+
+// fetch(url + page, options)
+//     .then((response) => response.json())
+//     .then((response) => {
+//         console.log(response);
+//         const posts = response['results'];
+//         posts.forEach((post) => {
+//             // console.log(post['backdrop_path'], post['original_title'], post['overview'], post['poster_path']);
+//             console.log(post['id'], image_base_url + size + post['poster_path'], post['original_title'], post['overview'], post['vote_average']);
+//         })
+//     })
+//     .catch((err) => console.error(err));
+
+// function renderMovieTemplate(postId, postImg, postTitle, postOverView, postVoteAvg) {
+//     let template = `
+//         <div id=${postId}>
+//             <div>
+//                 <img src=${image_base_url}${size}${postImg} />
+//             </div>
+//             <div>
+//                 <h2>${postTitle}</h2>
+//                 <div>
+//                     ${postOverView}
+//                 </div>
+//                 <span>${postVoteAvg}</span>
+//             </div>
+//         </div>
+//     `;
+
+//     return template;
+// }
